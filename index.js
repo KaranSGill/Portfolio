78eb4b40e9ba27214249a37699538e83215a0b4b
Karanbir Singh <71957360+KaranSGill@users.noreply.github.com>
2021-10-19 14:41:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-console.log('Hello World');
-console.log(2+3);
-let x= 2;
-let y= 4;
 
-if (x>y){
-    console.log(`NO it is not a case the value for x is ${x}`);
-}
-else{
-console.log(`The value of y is bigger and it is ${y}`);
-}
-
-console.log(`My name is karanbir Singh and the value of x is ${y}`);
